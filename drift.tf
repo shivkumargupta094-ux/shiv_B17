168422c9493ed7ecdcfa537fa5c4c0e6366accb1
shivkumargupta094-ux <shivkumargupta094@gmail.com>
2026-03-30 16:15:09 +0530

    rg added

---
+++ drift.tf
@@ -0,0 +1,18 @@
+terraform {
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "4.66.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "sanjay_rg"
+  location = "eastus"
+}
